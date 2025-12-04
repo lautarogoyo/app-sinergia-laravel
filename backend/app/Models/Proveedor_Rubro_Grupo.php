58bd6a6f4d9dc506cb1547e93988df5077e6b4a3
lautarogoyo <127627677+lautarogoyo@users.noreply.github.com>
2025-12-04 11:47:21 -0300

    Model de proveedor grupo rubro

--- backend/app/Models/Proveedor_Rubro_Grupo.php
+++ backend/app/Models/Proveedor_Rubro_Grupo.php
@@ -9,4 +9,27 @@ class Proveedor_Rubro_Grupo extends Model
 {
     /** @use HasFactory<\Database\Factories\ProveedorRubroGrupoFactory> */
     use HasFactory;
+    protected $table = 'proveedor_rubro_grupo';
+    protected $fillable = [
+        'descripcion',
+        'id_rubro',
+        'id_proveedor',
+        'id_grupo'
+    ];
+
+    public function rubro() : BelongsTo
+    {
+        return $this->belongsTo(Rubro::class);
+    }
+
+    public function proveedor() : BelongsTo
+    {
+        return $this->belongsTo(Proveedor::class);
+    }
+
+    public function grupo() : BelongsTo
+    {
+        return $this->belongsTo(Grupo::class);
+    }
+
 }
