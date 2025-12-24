a24686bd94d09efee87fc7528508170e93add145
lautarogoyo <127627677+lautarogoyo@users.noreply.github.com>
2025-12-24 11:09:20 -0300

    Limpieza de codigo

--- frontend/src/componentes/Fetch/get.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-export function getData(url, objectKey) {
-  return axios.get(url)
-    .then((response) => response.data[objectKey]);
-}   
--- frontend/src/componentes/Fetch/getOne.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-export function getOne(url, objectKey) {
-  return axios.get(url)
-    .then((response) => response.data[objectKey]);
-}   
