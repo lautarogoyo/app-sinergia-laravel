7803e00391eecec3be0266770d2d51e2e1908fb2
lautarogoyo <127627677+lautarogoyo@users.noreply.github.com>
2026-03-28 11:32:02 -0300

    feature(login-front): Realizado el frontend del login

--- frontend/src/componentes/App/App.jsx
+++ frontend/src/componentes/App/App.jsx
@@ -1,13 +1,21 @@
 import Header from '../Templates/Header.jsx';
 import AppRouter from '../Router/AppRouter.jsx';
+import Login from '../Login/Login.jsx';
 
 
 export default function App() {
   
   return (
-    <div className="flex">
-      <Header />
-      <AppRouter />
-    </div>
+    <>
+      <div className=''>
+        <Login />
+      </div>
+      
+      <div className="flex">
+        <Header />
+        <AppRouter />
+      </div>
+    </>
   );
+
 }
