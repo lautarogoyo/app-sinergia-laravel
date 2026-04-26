60b5022e719c649d87cee868be404261b35421e5
lautarogoyo <127627677+lautarogoyo@users.noreply.github.com>
2026-04-26 14:34:48 -0300

    fix(router): correccion en el app router

--- frontend/src/componentes/Router/AppRouter.jsx
+++ frontend/src/componentes/Router/AppRouter.jsx
@@ -8,16 +8,9 @@ import EditDocument from "../Empleados/EditDocument.jsx";
 import Obras from "../Obras/Obras.jsx";
 import CreateObra from "../Obras/CreateObra.jsx";
 import EditObra from "../Obras/EditObra.jsx";
-import Grupos from "../Personas/Grupos/Grupos.jsx";
-import CreateGrupo from "../Personas/Grupos/CreateGrupo.jsx";
-import EditGrupo from "../Personas/Grupos/EditGrupo.jsx";
-import RemoveGrupo from "../Personas/Grupos/RemoveGrupo.jsx";
 import Gestionar from "../Obras/Gestionar.jsx";
 import DiagramaObras from "../Obras/DiagramaObras.jsx";
 import Personas from "../Personas/Personas.jsx";
-import CreatePersona from "../Personas/Proveedores/CreatePersona.jsx";
-import EditPersona from "../Personas/Proveedores/EditPersona.jsx";
-import RemovePersona from "../Personas/Proveedores/RemovePersona.jsx";
 
 function AppRouter() {
     return (
@@ -29,22 +22,13 @@ function AppRouter() {
             <Route path="/obras" element={<Obras />} />
             <Route path="/obras/diagrama" element={<DiagramaObras />} />
             <Route path="/personas" element={<Personas />} />
-            <Route path="/crear-persona" element={<CreatePersona />} />
-            <Route path="/editarpersona/:id" element={<EditPersona />} />
-            <Route path="/eliminarpersona/:id" element={<RemovePersona />} />
             <Route path="/crear-obra" element={<CreateObra />} />
             <Route path="/editarobra/:id" element={<EditObra />} />
-            <Route path="/grupos" element={<Grupos />} />
             <Route path="/crear-empleado" element={<CreateEmpleado />} />
             <Route path="/editarempleado/:id" element={<EditeEmpleado />} />
             <Route path="/eliminarempleado/:id" element={<RemoveEmpleado />} />
-            <Route path="/crear-grupo" element={<CreateGrupo />} />
-            <Route path="/editargrupo/:id" element ={<EditGrupo />} />
-            <Route path="/eliminargrupo/:id" element ={<RemoveGrupo />} />
             <Route path="/documentacionempleado/:id" element={<EditDocument />} />
-            <Route path="/obras" element={<div>Obras</div>} />
             <Route path="/obra/:id/gestionar" element={<Gestionar />} />
-            <Route path="/empleados" element={<div>Empleados</div>} />
             <Route path="/salir" element={<Navigate to="/home" replace />} />
         </Routes>
     );
