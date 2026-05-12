d875f9183aad8c2edf6b351c4f33989ede94384e
lautarogoyo <lgoyoaga@frro.utn.edu.ar>
2026-05-12 12:01:25 -0300

    fix(front): arreglo de fetch

--- frontend/src/componentes/api/tipodocumento.js
+++ frontend/src/componentes/api/tipodocumento.js
@@ -3,6 +3,6 @@ const backendUrl = import.meta.env.VITE_API_URL;
 
 
 export const fetchTipoDocumento = async () => {
-    const { data } = await axios.get(`${backendUrl}/api/tipos_documento`);
+    const { data } = await axios.get(`${backendUrl}/api/tipos_documentacion`);
     return data;
 };
